939de476f41b688b069b1ddd3f3ece3f54238282
utumiao <utumiaoine@gmail.com>
2026-01-28 09:15:17 +0900

    Update kmms.js

--- kmms.js
+++ kmms.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.getElementById("start-button");
   const skipButton = document.getElementById("skip-button");
   const errorMessage = document.getElementById("error-message");
-
+const scenarioBox = document.getElementById("scenario-box");
+  
   const inputs = [
     document.getElementById("PC1-myouji"),
     document.getElementById("PC1-namae"),
